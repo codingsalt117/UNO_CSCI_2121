f4f935c8608433adcde1a9d67fe4d637443d55f4
rwjohns1 <rwjohns1@uno.edu>
2022-04-02 18:55:31 -0500

    Recursion with comments

--- Lab6/Recursion.java
+++ Lab6/Recursion.java
@@ -8,18 +8,20 @@
 public class Recursion {
     
     public static String reverse(String s) {
-       
+       // Checks if string is empty
        if (s.isEmpty()){
             return s;
        } 
+       // If not empty string is passed to the recursion step the reorders the first two indicies until string is in reverse order
         return reverse(s.substring(1)) + s.charAt(0);
     }
 
     public static boolean isPalindrome(String s) {
-        
+        // check if a string is reversed by calling reverse on "s" inside the equals call
         if (s.equals(reverse(s))){
             return true;
         }
+
         else {
             return false;
         }
@@ -27,11 +29,11 @@ public class Recursion {
     }
 
     public static int length(String s) {
-        
+        // Checks if string is empty
         if (s.equals("")){
             return 0;
         } 
-
+        // returns a int value of the length of the string by using substring method and a sort of iterator, for every element is a new recursive method all.
         else{
             return length(s.substring(1)) + 1;
         }
