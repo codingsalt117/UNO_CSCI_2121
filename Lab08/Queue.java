3caede922aee3e9d05f1d9b0d5cafa7a0b0240ee
rwjohns1 <rwjohns1@uno.edu>
2022-04-18 20:24:18 -0500

    Lab08 Files

---
+++ Lab08/Queue.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Queue<T> {
+
+	/**
+	 * Instantiate the needed datastructures in here (HINT: You might want to use
+	 * arraylist)
+	 */
+	private ArrayList<T> elements;
+
+	public Queue() {
+		this.elements = new ArrayList<T>();
+	}
+
+	/**
+	 * Inserts the specified element into this queue if it is possible to do so
+	 * immediately without violating capacity restrictions. When using a
+	 * capacity-restricted queue, this method is generally preferable to add(E),
+	 * which can fail to insert an element only by throwing an exception.
+	 *
+	 * @param element The element to add
+	 */
+	public void offer(T element) throws ClassCastException, NullPointerException, IllegalArgumentException {
+		this.elements.add(element);
+	}
+
+	/**
+	 * Retrieves, but does not remove, the head of this queue, or returns null if
+	 * this queue is empty.
+	 *
+	 * @return the head of the queue, or null if the queue is empty
+	 */
+	public T peek() throws NoSuchElementException, IndexOutOfBoundsException  {
+		if(elements.isEmpty()){
+			return null;
+		}
+		return elements.get(0);
+	}
+
+	/**
+	 * Retrieves and removes the head of this queue, or returns null if this queue
+	 * is empty.
+	 *
+	 * @return the head of the queue, or null if the queue is empty
+	 *
+	 */
+	public T poll() throws NoSuchElementException, IndexOutOfBoundsException { 
+		if(elements.isEmpty()){
+			return null;
+		}
+		return this.elements.remove(0);
+	}
+
+	public static void main(String[] args) {
+		Queue<String> queue = new Queue<String>();
+		queue.offer("This");
+		queue.offer("is");
+		queue.offer("a");
+		queue.offer("queue");
+
+		System.out.println(queue.peek());
+		System.out.println(queue.peek());
+		System.out.println(queue.peek() + System.lineSeparator());
+
+		System.out.println(queue.poll());
+		System.out.println(queue.poll());
+		System.out.println(queue.poll());
+		System.out.println(queue.poll() + System.lineSeparator());
+
+		System.out.println(queue.poll());
+		System.out.println(queue.peek());
+	}
+}
---
+++ Lab08/Stack.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ * 
+ */
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Stack<T> {
+
+	/** You might want to use arraylist for this too */
+	private ArrayList<T> elements;
+	
+	public Stack() {
+		this.elements = new ArrayList<T>();
+	}
+
+	
+	/**
+	 * Returns the number of components in this stack.
+	 *
+	 * @return the number of components in the stack
+	 */
+	public int getSize() {
+		
+		return this.elements.size();
+	}
+
+	
+
+	/**
+	 * Tests if this stack is empty.
+	 *
+	 * @return true if the stack is empty false otherwise
+	 */
+	public boolean isEmpty() {
+		
+		return this.elements.isEmpty();
+	}
+
+	
+	/**
+	 * Pushes an item onto the top of this stack.
+	 */
+	public void push(T item) {
+		this.elements.add(item);
+	
+	}
+
+
+
+	/**
+	 * Looks at the object at the top of this stack without removing it from the
+	 * stack.
+	 *
+	 * @return the object at the top of this stack
+	 */
+	public T peek() throws NoSuchElementException {
+		T element = this.elements.get(elements.size() - 1);
+		return element;
+	}
+
+	
+
+	/**
+	 * Removes the object at the top of this stack and returns that object as the
+	 * value of this function.
+	 *
+	 * @return The object at the top of this stack
+	 */
+	public T pop() throws EmptyStackException{
+		T popped;
+		popped = this.elements.get(elements.size() - 1);
+		this.elements.remove(elements.size() - 1);
+		return popped;
+	}
+
+	
+
+	/**
+	 * Prints each component of the stack seperated by a new line character.
+	 *
+	 * @returns The string representation of the stack
+	 */
+	@Override
+	public String toString() {
+		//need to have a for loop to return the values for print
+		String toString = "";
+
+		for(int i = 0; i < this.elements.size(); i++){
+			toString = toString + this.elements.get(i) + "\n";
+		}
+
+		return toString;
+	}
+
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<String>();
+		// Stack<Interger> stackInt = new Stack<Integer>();
+		// stackInt.push(10);
+		// System.out.println(stackInt.toString());
+		stack.push("This");
+		stack.push("is");
+		stack.push("a");
+		stack.push("stack");
+		System.out.println(stack.toString());
+
+		stack.pop();
+		stack.pop();
+		System.out.println(stack.toString());
+
+		stack.push("still");
+		stack.push("a");
+		stack.push("stack");
+		System.out.println(stack.toString());
+
+		System.out.println(stack.peek());
+		System.out.println(stack.peek());
+	}
+}
